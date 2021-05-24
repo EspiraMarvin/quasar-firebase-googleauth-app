c1c7cfad4310f1a90f8fc1bb05e0cc5ebe51f5bb
EspiraMarvin <espiramarvin@gmail.com>
2021-05-24 16:37:16 +0300

    second commit

---
+++ src/components/AuthComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="text-center q-mb-lg">Sign in with</div>
+    <div class="flex flex-center">
+      <q-btn
+        class="flex flex-center q-px-lg q-py-sm q-mb-md"
+        size="md"
+        label="Google"
+        @click="btnLogin"
+        color="primary"
+      />
+    </div>
+    <template  v-if="tab === 'register'">
+      <p class="text-center">Sign up with credentials</p>
+    </template>
+    <template v-else>
+      <p class="text-center">Sign in with credentials</p>
+    </template>
+    <q-form>
+      <q-input
+        v-model="formData.email"
+        outlined
+        class="q-mb-md"
+        type="email"
+        label="Email" />
+      <q-input
+        v-model="formData.password"
+        outlined
+        class="q-mb-md"
+        type="password"
+        label="Password" />
+      <div class="row">
+        <q-space />
+        <q-btn
+          type="submit"
+          color="primary"
+          :label="tab" />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthComponent",
+  props: ['tab'],
+  data (){
+    return {
+      formData: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    btnLogin () {
+      console.log('jahaha')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-page class="flex q-pa-md">
+    <q-card class="full-width">
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="login" label="Login" />
+        <q-tab name="register" label="Register" />
+      </q-tabs>
+
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="login">
+          <AuthComponent :tab="tab" />
+        </q-tab-panel>
+
+        <q-tab-panel name="register">
+          <AuthComponent :tab="tab"/>
+        </q-tab-panel>
+
+      </q-tab-panels>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import AuthComponent from "components/AuthComponent";
+export default {
+  name: 'PageIndex',
+  components: { AuthComponent },
+  data () {
+    return {
+      tab: 'login',
+      form: {
+        email: '',
+        password: ''
+      },
+      userAccountDialog: false,
+      resetPwdDialog: false,
+      dialogTitle: '',
+      method: ''
+    }
+  },
+  methods: {
+    btnLogin() {
+      this.userAccountDialog = true
+      this.method = 'sign in'
+      this.dialogTitle = 'Login Account'
+    }
+  }
+}
+</script>
