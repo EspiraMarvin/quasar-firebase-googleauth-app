0237f91c6f941cef0e86e69cb9759c5de0fc4096
EspiraMarvin <espiramarvin@gmail.com>
2021-05-31 23:24:51 +0300

    firebaseconfig vars

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,22 +3,13 @@ import firebase from "firebase";
 //set vars for process.env
 const firebaseConfig = {
     apiKey: "AIzaSyCYSpgPVmvZYZNrWz2whxMcQ0qulqQ1wx4",
-    // apiKey: process.env.VUE_API_KEY,
-    // apiKey: process.env.API_KEY,
     authDomain: "quasar--auth.firebaseapp.com",
-    // authDomain: process.env.AUTH_DOMAIN,
     projectId: "quasar--auth",
-    // projectId: process.env.PROJECT_ID,
     storageBucket: "quasar--auth.appspot.com",
-    // storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: "608023091006",
-    // messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: "1:608023091006:web:abb0169ff10f48c8173e6f"
-    // appId: process.env.APP_ID
   };
   firebase.initializeApp(firebaseConfig);
-  // console.log('firebaseConfig', firebaseConfig),
-  // console.log('vue app mode', process.env.VUE_APP_MODE)
 
 
 firebase.getCurrentUser = () => {
