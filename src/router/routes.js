ee6e6dbb6a7e8413e42a9bc85e4ba12becc47b66
EspiraMarvin <espiramarvin@gmail.com>
2021-07-14 02:52:22 +0300

    router update

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Auth.vue') },
+      { path: '/', component: () => import('pages/Auth.vue') },
       { path: '/home', component: () => import('pages/Home.vue'), meta: {requiresAuth: true} }
     ]
   },
